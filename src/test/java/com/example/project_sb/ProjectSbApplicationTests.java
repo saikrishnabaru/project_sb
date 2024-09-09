30c79c1fb4527e0f217bfe6c9cc9e5d4f73cddb9
Sai Krishna Barupati <saikrishnabarupati120@gmail.com>
2024-09-09 02:37:27 -0400

    resolved RTE

--- src/test/java/com/example/project_sb/ProjectSbApplicationTests.java
+++ src/test/java/com/example/project_sb/ProjectSbApplicationTests.java
@@ -1,10 +1,14 @@
 package com.example.project_sb;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ProjectSbApplicationTests {
