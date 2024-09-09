a2fc39acb11dc5f0dd3c26c31ff78a451e742fe9
Sai Krishna Barupati <saikrishnabarupati120@gmail.com>
2024-09-09 01:21:12 -0400

    first commit

---
+++ src/main/java/com/example/project_sb/ProjectSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSbApplication.class, args);
+	}
+
+}
